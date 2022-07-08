71578d3dd7fccde35213f401efe696dd9c2c15b9
TeeTru <teranzbm@gmail.com>
2022-07-08 12:09:11 -0600

    inheritance assignment

---
+++ InheritanceAssignment.py
@@ -0,0 +1,16 @@
+
+
+class vehicle:
+    #defines attributes of class
+    engine = ""
+    physical_form = ""
+    number_of_doors = ""
+    speed = ""
+
+class Airplane(vehicle):
+    number_of_wings: ""
+    number_of_passangers: ""
+
+class Helocopter(vehicle):
+    number_of_propellers: ""
+    number_of_engines: ""
